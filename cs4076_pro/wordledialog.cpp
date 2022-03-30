dd090d9532955a927ba35bb3fb8a2391cfc90b71
7LuxinC <20239696@studentmail.ul.ie>
2022-03-30 14:16:51 +0100

    processing wordle

--- cs4076_pro/wordledialog.cpp
+++ cs4076_pro/wordledialog.cpp
@@ -1,14 +1,63 @@
 #include "wordledialog.h"
 #include "ui_wordledialog.h"
+#include <QMessageBox>
+#include <string>
+#include<QString>
+using namespace std;
+
+int counter = 0;
 
 WordleDialog::WordleDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::WordleDialog)
 {
     ui->setupUi(this);
+
+
 }
 
 WordleDialog::~WordleDialog()
 {
     delete ui;
 }
+
+
+
+
+
+void WordleDialog::on_lineEdit_returnPressed()
+{
+
+
+
+    QString textInLine = ui ->lineEdit->text();
+
+
+        ui -> opText ->append("You Entered:  " + textInLine);
+        ui->lineEdit ->clear();
+
+        ui ->typedWords ->append(textInLine);
+
+        counter++;
+
+        if(counter == max){
+            ui ->lineEdit->setEnabled(false);
+            QMessageBox::information(this," Challenge Ended ", "Challenge is ended,you cannot fill up or activate the magic potion. Try next time.");
+
+        }
+
+
+}
+
+
+
+
+
+void WordleDialog::on_pushButton_clicked()
+{
+    QString qstr = QString::fromStdString("Wordle is simple: You have six chances to guess the day's secret five-letter word. Type in a word as a guess, and the game tells you which letters are or aren't in the word. The aim is to figure out the secret word with the fewest guesses."
+                                          "If player won the game, the magic potion get activate or fill up. If you lost, you cannot use potion to activate the power of 5-colours sprite stone");
+    QMessageBox::information(this," Wordle ", qstr);
+
+}
+
--- cs4076_pro/wordledialog.h
+++ cs4076_pro/wordledialog.h
@@ -2,6 +2,7 @@
 #define WORDLEDIALOG_H
 
 #include <QDialog>
+#include <string>
 
 namespace Ui {
 class WordleDialog;
@@ -15,8 +16,16 @@ public:
     explicit WordleDialog(QWidget *parent = nullptr);
     ~WordleDialog();
 
+private slots:
+    void on_lineEdit_returnPressed();
+
+
+
+    void on_pushButton_clicked();
+
 private:
     Ui::WordleDialog *ui;
+    const int max = 6;
 };
 
 #endif // WORDLEDIALOG_H
