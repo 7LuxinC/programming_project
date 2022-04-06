22071b74eb97244e0a8e67cd0c103cb12de421c8
7LuxinC <20239696@studentmail.ul.ie>
2022-04-06 17:38:53 +0100

    .

--- build-cs4076_project-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/debug/moc_wordledialog.cpp
+++ build-cs4076_project-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/debug/moc_wordledialog.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../cs4076_pro/wordledialog.h"
+#include "../../../../../Downloads/programming_project/cs4076_pro/wordledialog.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
